874236866e18eeeca7aaf3e53fd28ce75df45b95
funcy2267 <77931709+funcy2267@users.noreply.github.com>
2021-03-24 01:00:57 +0100

    Add tabs instead of spaces

--- launcher.py
+++ launcher.py
@@ -5,15 +5,17 @@ addon = xbmcaddon.Addon()
 
 def launch():
 
-    addonPath = addon.getAddonInfo('path')
+	addonPath = addon.getAddonInfo('path')
 
-    cmd = os.path.join(addonPath, 'script.sh')
+	cmd = os.path.join(addonPath, 'script.sh')
 
-    try:
-        xbmc.log("Launching script %s" % cmd)
-        subprocess.call(["chmod", "+x", cmd]) # make sure it's executable
-        subprocess.call('"' + cmd + '"', shell=True)
-    except:
-        xbmcgui.Dialog().ok("Failed to launch", "Failed to launch script %s" % cmd)
+	try:
+		xbmc.log("Launching script %s" % cmd)
+		subprocess.call(["chmod", "+x", cmd])
+		subprocess.call('"' + cmd + '"', shell=True)
+	except:
+	xbmcgui.Dialog().ok("Failed to launch", "Failed to launch script %s" % cmd)
 
 launch()
+
+# Thanks to shrimpza for providing this script [https://github.com/shrimpza/kodi-addons/blob/master/script.launcher.firefox/launcher.py]
